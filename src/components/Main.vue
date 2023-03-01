2302e111ca355ba56d3023d788b622052457db58
SaraG94 <sara.gherbassi.94@gmail.com>
2023-03-01 20:06:46 +0100

    FEAT content nav+main and style

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -11,32 +11,80 @@ export default{
 <template>
 
     <main>
-        <div class="main-content">
-            {{message}}
-        </div>
-
         <div class="container">
-            <div class="nav-main"></div>
+            <div class="main-content">
+                {{message}}
+            </div>
+
+            <div class="shop-items">
+                <ul class="items">
+                    <li>
+                        <img src="/img/buy-comics-digital-comics.png" alt="">
+                        <p>Digital comics</p>
+                    </li>
+                    <li>
+                        <img src="/img/buy-comics-merchandise.png" alt="">
+                        <p>Dc merchandise</p>
+                    </li>
+                    <li>
+                        <img src="/img/buy-comics-subscriptions.png" alt="">
+                        <p>subscription</p>
+                    </li>
+                    <li class="shop">
+                        <img src="/img/buy-comics-shop-locator.png" alt="">
+                        <p>comic shop locator</p>
+                    </li>
+                    <li>
+                        <img src="/img/buy-dc-power-visa.svg" alt="">
+                        <p>dc power visa</p>
+                    </li>
+                </ul>
+            </div>
         </div>
     </main>
 
 </template>
 
 <style scoped>
+    .container{
+        background-color: #0282F9;
+        color: white;
+    }
 
-  .main-content{
-    /*debug*/
-    min-height: 100px;
-    min-width: 1280px;
-    background-color: black;
-    color: white;
-  }
+    .main-content{
+        min-height: 100px;
+        background-color: black;
+        color: white;
+    }
 
- .container{
-    /*debug*/
-    min-height: 100px;
-    min-width: 1280px;
-    background-color: #0282F9;
-    color: white;
-  }
+    .shop-items{
+        display: flex;
+        align-items: center;
+        padding: 45px 0;
+    }
+
+    ul.items{
+        list-style: none;
+        margin:0;
+        width:100%;
+        display:flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    ul.items>li{
+        display: flex;
+        align-items: center;
+        gap: 20px;
+        text-transform: uppercase;
+        font-size: 15px;
+    }
+
+    ul.items img{
+        width: 50px;
+    }
+
+    li.shop img{
+        width:35px;
+    }
 </style>
--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -11,17 +11,47 @@ export default{
 <template>
 
   <div class="nav-header">
-  
+    <div class="logo">
+      <img src="/img/dc-logo.png" alt="">
+    </div>
+    <ul class="nav-items">
+      <li>Characters</li>
+      <li>Comics</li>
+      <li>Movies</li>
+      <li>TV</li>
+      <li>Games</li>
+      <li>Collectibles</li>
+      <li>Videos</li>
+      <li>Fans</li>
+      <li>News</li>
+      <li>Shop</li>
+    </ul>
   </div>
 
 </template>
 
 <style scoped>
-  .nav-header{
-    /*debug*/
-    min-height: 100px;
-    min-width: 1280px;
-    background-color: bisque;
+  ul{
+    list-style: none;
   }
+
+  img{
+    max-width:100%;
+  }
+
+  .nav-header{
+    display:flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .nav-items{
+    display: flex;
+    gap: 20px;
+    text-transform: uppercase;
+    font-weight: 700;
+  }
+
+  
 
 </style>
