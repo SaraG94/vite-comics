5c51a2af0750d183e0d341a5461e300546570614
SaraG94 <sara.gherbassi.94@gmail.com>
2023-03-01 17:29:48 +0100

    FEAT nav-header-macro+debug Style

---
+++ src/components/NavHeader.vue
@@ -0,0 +1,25 @@
+<script >
+export default{
+  data(){
+
+  },
+}
+</script>
+
+<template>
+
+  <div class="nav-header">
+  
+  </div>
+
+</template>
+
+<style scoped>
+  .nav-header{
+    /*debug*/
+    min-height: 100px;
+    min-width: 1280px;
+    background-color: bisque;
+  }
+
+</style>
